65c890613dab57f1563aa78a64ab7255a3a2e2d8
Prutfugl <Prutfugl789@gmail.com>
2026-03-04 12:49:00 +0100

    Commit_4

--- Winemaker.py
+++ Winemaker.py
@@ -16,26 +16,24 @@ def LavVinGlas():
 
     minModel1 = solid2.sphere(20)
 
-    minModel2 = solid2.cube([40,50,100], center=True)
+    minModel2 = solid2.cube([40,50,30], center=True)
     minModel2 = solid2.translate([0,0,VinHul])(minModel2)
 
     minModel3 = solid2.sphere(19)
 
     minModel = minModel1 - minModel2- minModel3
-    minModel = solid2.translate([0,0,20])(minModel)
+    minModel = solid2.translate([0,0,19+VinHojde/2])(minModel)
 
     minModel4 = solid2.cylinder(h=VinHojde, r=5,center=True)
-    minModel4 = solid2.translate([0,0,-8])(minModel4)
 
     minModel5 = solid2.cylinder(h=5,r1=VinBund,r2=5)
-    minModel5 = solid2.translate([0,0,-20])(minModel5)
+    minModel5 = solid2.translate([0,0,-VinHojde/2])(minModel5)
 
     Text = solid2.text(text=VinNavn, size=5)
-    Text = solid2.translate([-8,-3,-20])(Text)
-    Text = solid2.mirror([0,1,0])(Text)
+    Text = solid2.translate([-8,-3,2+VinHojde/2])(Text)
     #Text = solid2.linear_extrude(height=5)(Text)
 
-    finalModel = minModel + minModel4 + minModel5 - Text
+    finalModel = minModel + minModel4 + minModel5 + Text
 
     if(VinHojde >= 20 or VinHojde <= 5):
         FejlBesked.config(text="Fejl, højde skal være mellem 5 og 20",fg="red")
